292b66d63e706871ad2e68987d535af663a7bc35
Viktor2025-bit <kaluvictor130@gmail.com>
2025-02-12 16:40:40 +0100

    Update router.py

--- api/router.py
+++ api/router.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
-
-from api.routes import books
+from api.endpoints.books import router as books_router  
 
 api_router = APIRouter()
-api_router.include_router(books.router, prefix="/books", tags=["books"])
+api_router.include_router(books_router, prefix="/books", tags=["Books"])
